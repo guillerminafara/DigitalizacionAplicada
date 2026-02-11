daa3e71787944052dee2840391e7ddf21006c81f
guillermina <faramagui@hotmail.com>
2026-02-11 13:22:40 +0100

    subir tareas

--- RedNeuronal/apoyo/MINST.py
+++ RedNeuronal/apoyo/MINST.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +8,34 @@ CORS(app)  # Habilitar CORS
 
 # Cargar el modelo completo
 model = tf.keras.models.load_model('modelo_MINST_completo.keras')
+
+
+def preprocess_canvas(img):
+    # INVERTIR COLORES
+    img= 255-img
+    #Binariza
+    _, img= cv2.theshold(img, 50, 255, cv2.THRESH_BINARY)
+
+    #bounding box 
+    coords= cv2.findNonZero(img)
+    if coords is not None:
+        x,y,w,h=cv2.boundingRect(coords)
+        img=img[y:y+h, x:x+w]
+    
+    #hacer cuadrado 
+    size=max(img.shape)
+    square=np.zeros((size,size), dtype=np.uint8)
+
+    #resize MNIST
+    img=cv2.resize(square, (28,28))
+
+    #normalizar
+    img= img.astype("float32")/255.0
+
+    #shape final 
+    img=img=reshape(1,28,28,1)
+
+    return img
 
 def predecir_digito(pixeles):
     # Convertir string de pixeles a array numpy
